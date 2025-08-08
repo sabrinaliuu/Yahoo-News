da4985db4da38f8862b81ad82498e189f5aa50d5
sabrinaliuu <106926085+sabrinaliuu@users.noreply.github.com>
2025-08-08 11:42:05 +0900

    update comment

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ keywords = '能登半島地震+復興'
 
 ##### start controlled mode #####
 driver = webdriver.Chrome()
-driver.get('https://news.yahoo.co.jp/search?p='+keywords+'&ei=utf-8') # put the url you want to get
+driver.get('https://news.yahoo.co.jp/search?p='+keywords+'&ei=utf-8') # put the url you want to get # go to this page
 
 
 ## 2. Load existing news articles ##
@@ -24,9 +24,9 @@ while True: #page_count<6:
     print(page_count)
     try:
         motto_bttn  = driver.find_element(By.CLASS_NAME, "sc-1hdlpx9-1.hzrbWI") # find button
-        motto_bttn.click()
+        motto_bttn.click() # click the button
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") # scroll down
-        time.sleep(8)
+        time.sleep(8) # wait for 8 seconds (loading)
         page_count += 1
         
         if page_count%10 == 0:
@@ -37,14 +37,14 @@ while True: #page_count<6:
 
 ## 3. Get news articls list ##
 ##### get titles & urls #####
-soup = BeautifulSoup(driver.page_source, 'lxml')
-rows = soup.find_all('li',class_=re.compile("sc-1u4589e-0 kKmBYF"))
+soup = BeautifulSoup(driver.page_source, 'lxml') # get the page
+rows = soup.find_all('li',class_=re.compile("sc-1u4589e-0 kKmBYF")) # find all news
 titles = list()
 urls = list()
 times = list()
-for r in rows:
+for r in rows: # get title, url for all news by row (by element li)
     title = r.find(class_="sc-3ls169-0 sc-110wjhy-2 dHAJpi dKognN").text
-    url = r.find('a').get('href')
+    url = r.find('a').get('href') # save url # important !! # you need this to go into each news article's page
     time_ = r.find('time').text
     titles.append(title)
     urls.append(url)
@@ -69,21 +69,21 @@ for p_num in range(0,post_num):
     
     try:
         post_new = '' 
-        u = urls[p_num]
-        driver.get(u)
+        u = urls[p_num] # load each url
+        driver.get(u) # go to the page by url
         soup_each = BeautifulSoup(driver.page_source, 'lxml')
-        pages = soup_each.find_all(class_="sc-qkog50-0 lfMsek")
+        pages = soup_each.find_all(class_="sc-qkog50-0 lfMsek") # check how many pages for this news articles
         page_num = 1 if len(pages)==0 else len(pages)
         
-        for page in range(1,page_num+1):
+        for page in range(1,page_num+1): # load all texts for each page for this news articles
 
             driver.get(u+'?page='+str(page))
-            time.sleep(8)
+            time.sleep(8) # wait
             soup_each = BeautifulSoup(driver.page_source, 'lxml')
                 
             post = soup_each.find_all(class_="sc-54nboa-0 deLyrJ yjSlinkDirectlink highLightSearchTarget")#"article_body highLightSearchTarget").text#"sc-54nboa-0 deLyrJ yjSlinkDirectlink highLightSearchTarget").text
             for p in post:
-                post_new = post_new + p.text + '\n\n'
+                post_new = post_new + p.text + '\n\n' # combine texts
  
         times_new.append(times[p_num])
         urls_new.append(urls[p_num])
